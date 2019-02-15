baf7480562a4d63e7a9ea437d843ac75789eafc2
Jia Shi <shijia1019@gmail.com>
2019-02-15 14:25:31 -0600

    scripts to run

---
+++ demos/Dev.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH -J s1p1C3k
+#SBATCH -o s1p1C3k_%j.txt
+#SBATCH -e errs1p1C3k_%j.err
+#SBATCH -p skx-dev
+##SBATCH --ntasks=32
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=48
+##SBATCH -c 4
+#SBATCH --export=ALL
+#SBATCH --time=2:00:00
+#SBATCH -A TG-EAR170019
+
+export OMP_NUM_THREADS=2
+export MV2_ENABLE_AFFINITY=0
+
+cd /work/04149/tg834482/stampede2/release0/NormalModes/
+source setEnv
+cd demos/
+#ibrun valgrind --error-limit=no ./plmvcg_stampede2.out 
+ibrun ./../bin/plmvcg_stampede2.out 
